e70ea84ecfd0091e99201585462829117f3b5251
Hakim Benoudjit <h.benoudjit@gmail.com>
2018-02-12 02:09:06 +0000

    Make header & footer responsive

--- app/base/base.component.ts
+++ app/base/base.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+import { ObservableMedia } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-base',
@@ -11,7 +12,7 @@ export class BaseComponent implements OnInit {
     slogan = 'GIS Tutorials For Everyone';
     date = new Date();
 
-    constructor(private googleAnalytics: Angulartics2GoogleAnalytics) { }
+    constructor(private googleAnalytics: Angulartics2GoogleAnalytics, public media: ObservableMedia) { }
 
     ngOnInit() {
     }
