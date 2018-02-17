bcec02a463d7118ef8afc6f3ad6317303bb5c401
Hakim Benoudjit <h.benoudjit@gmail.com>
2018-02-17 21:33:18 +0000

    Fix scroll problem with sidenav

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { URL, MONTHS } from './constants';
-import { MediaChange, ObservableMedia } from '@angular/flex-layout';
 import { Tag } from './models/article.model';
+import { ObservableMedia } from '@angular/flex-layout';
 
 
 @Component({
@@ -18,22 +18,13 @@ export class AppComponent {
     opened: boolean = true;
     
     // inject classes
-    constructor(private route: ActivatedRoute, private router: Router,
-                private media: ObservableMedia) { }
+    constructor(private route: ActivatedRoute, private router: Router, public media: ObservableMedia) { }
 
     ngOnInit() {
         // get months containing articles through resolve
         this.route.data.subscribe((data) => {
             this.months = data.months;
             this.tags = data.tags;
-        });
-
-        // close sidebar if media is mobile size
-        this.media.subscribe((change: MediaChange) => {
-            if (this.media.isActive('xs'))
-                this.opened = false;
-            else
-                this.opened = true;
         });
     }
 }
